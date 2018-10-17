663fbd75a05f83d4493ba8117781c8a368d58c01
Ricardo Paiva <ricardoccpaiva@gmail.com>
2018-10-17 12:15:51 +0100

    Add production database config

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,12 @@ config :montacargas, MontaCargasWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :montacargas, MontaCargas.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
