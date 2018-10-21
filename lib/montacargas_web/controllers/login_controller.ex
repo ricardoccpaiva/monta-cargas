b0d8c03f8ac2cce585f8dbf6a715966987b0a995
Ricardo Paiva <ricardoccpaiva@gmail.com>
2018-10-21 10:24:39 +0100

    First draft of tabler-ui template

--- lib/montacargas_web/controllers/login_controller.ex
+++ lib/montacargas_web/controllers/login_controller.ex
@@ -2,6 +2,6 @@ defmodule MontaCargasWeb.LoginController do
   use MontaCargasWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", layout: {MontaCargasWeb.LayoutView, "login.html"})
   end
 end
